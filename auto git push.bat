b88644158454b8a6499f1f7c6f807acb2fdc292d
safder <107740161+safder2000@users.noreply.github.com>
2022-12-19 16:32:55 +0530

    Delete auto git push.bat

--- auto git push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p input=Enter the text for the Markdown file:
-echo %input% > file.md
-git init
-git remote add origin https://github.com/safder2000/md-saves.git
-git add .
-git commit -m "Adding file.md"
-git push -u origin master
